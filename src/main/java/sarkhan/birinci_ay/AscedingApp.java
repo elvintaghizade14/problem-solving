cfe057bcc119a9486c4876c81be2a0add64c0604
Elvin <elviuntaghiyev1842gmail.com>
2020-04-03 17:18:27 +0400

    initial commit

---
+++ src/main/java/sarkhan/birinci_ay/AscedingApp.java
@@ -0,0 +1,29 @@
+package sarkhan.birinci_ay;
+
+import java.util.Scanner;
+
+public class AscedingApp {
+  private static final Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    // 1. input
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    int d = sc.nextInt();
+
+    // 2. process
+    boolean ans = isAscOrd(a, b, c, d);
+
+    // 3. output
+    System.out.println(ans ? "in ascending order" : "not ascending order");
+  }
+
+  private static boolean isAscOrd(int a, int b, int c, int d) {
+    int[] nums = {a, b, c, d};
+    for (int i = 0; i < 3; i++) {
+      if (nums[i] > nums[i + 1]) return false;
+    }
+    return true;
+  }
+}
---
+++ src/main/java/sarkhan/birinci_ay/CharAtIndexApp.java
@@ -0,0 +1,23 @@
+package sarkhan.birinci_ay;
+
+import java.util.Scanner;
+
+public class CharAtIndexApp {
+  private static final Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    // 1. input
+    String line = sc.nextLine();
+    int index = sc.nextInt();
+
+    // 2. process
+    char c = charAtIndex(line, index);
+
+    // 3. output
+    System.out.println(c);
+  }
+
+  private static char charAtIndex(String line, int index) {
+    return line.charAt(index);
+  }
+}
