b6f3009a9ea7689d589ec6877a9931448b6efef3
Elvin <elviuntaghiyev1842gmail.com>
2020-04-03 17:38:53 +0400

    initial commit

---
+++ src/main/java/hackerrank/java/JDateTime.java
@@ -0,0 +1,26 @@
+package hackerrank.java;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class JDateTime {
+  private static final Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    // 1. input
+    int m = sc.nextInt();
+    int d = sc.nextInt();
+    int y = sc.nextInt();
+
+    // 2. process
+    String res = findDay(m, d, y);
+
+    // 3. output
+    System.out.println(res);
+  }
+
+  static String findDay(int m, int d, int y) {
+    LocalDate ld = LocalDate.of(y, m, d);
+    return ld.getDayOfWeek().toString();
+  }
+}
