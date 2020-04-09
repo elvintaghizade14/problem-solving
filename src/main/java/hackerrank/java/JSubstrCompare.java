5f8cf55d5e9ac390d218b811c0cceedeb4a3d608
Elvin <elviuntaghiyev1842gmail.com>
2020-04-09 12:05:59 +0400

    initial commit

--- src/main/java/hackerrank/java/JSubstrCompare.java
+++ src/main/java/hackerrank/java/JSubstrCompare.java
@@ -1,5 +1,8 @@
 package hackerrank.java;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class JSubstrCompare {
@@ -8,10 +11,13 @@ public class JSubstrCompare {
   public static void main(String[] args) {
     String s = sc.next();
     int k = sc.nextInt();
-    System.out.print(comp(s, k));
-  }
-
-  private static String comp(String s, int k) {
-    throw new IllegalArgumentException("Not implemented yet...");
+    String[] subs = new String[s.length() - k + 1];
+    for (int i = 0; i < subs.length; i++) {
+      subs[i] = s.substring(i, i + k);
+    }
+    String max = Arrays.stream(subs).max(Comparator.naturalOrder()).orElseThrow(NoSuchElementException::new);
+    String min = Arrays.stream(subs).min(Comparator.naturalOrder()).orElseThrow(NoSuchElementException::new);
+    System.out.println(max);
+    System.out.println(min);
   }
 }
