6b821b48726406ff9f1a6fc6a57dc35ef2f2cfb8
Elvin <elviuntaghiyev1842gmail.com>
2020-04-09 18:29:12 +0400

    initial commit

--- src/main/java/project_euler/P3_LPF.java
+++ src/main/java/project_euler/P3_LPF.java
@@ -1,16 +1,27 @@
 package project_euler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class P3_LPF {
   public static void main(String[] args) {
+//    long n = 13195L;
     long n = 600851475143L;
-    boolean flag = false;
-    while (!flag) {
-      n--;
-      for (int i = 2; i <= Math.sqrt(n); i++) {
-        if (n % 2 == 0) break;
-      }
-      flag = true;
-    }
     System.out.println(n);
+
+    List<Integer> pfs = findPrimeFactors(n);
+    System.out.println(pfs);
+  }
+
+  private static List<Integer> findPrimeFactors(long num) {
+    List<Integer> pfs = new ArrayList<>();
+    for (int i = 2; i <= num; i++) {
+      if (num % i == 0) {
+        num /= i;
+        pfs.add(i);
+        i--;
+      }
+    }
+    return pfs;
   }
 }
