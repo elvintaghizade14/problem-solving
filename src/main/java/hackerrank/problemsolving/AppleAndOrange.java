3511202f8727d6c6ad9c48362c6bc8a1f7071ca0
Elvin <elvintaghiyev184@gmail.com>
2020-04-21 07:52:13 +0400

    "initial commit"

---
+++ src/main/java/hackerrank/problemsolving/AppleAndOrange.java
@@ -0,0 +1,19 @@
+package hackerrank.problemsolving;
+
+import java.util.Arrays;
+
+public class AppleAndOrange {
+
+  public static void main(String[] args) {
+    countApplesAndOranges(7, 11, 5, 15, new int[]{-2, 2, 1}, new int[]{5, -6});
+  }
+
+  static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+    System.out.println(countFruits(s, t, a, apples));
+    System.out.println(countFruits(s, t, b, oranges));
+  }
+
+  private static int countFruits(int houseS, int houseE, int k, int[] fruits) {
+    return (int) Arrays.stream(fruits).filter(value -> (k + value) >= houseS && (k + value) <= houseE).count();
+  }
+}
