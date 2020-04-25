998b6aec0a830758bc3be10100dd2636ada64717
Elvin <elvintaghiyev184@gmail.com>
2020-04-25 14:20:56 +0400

    "initial commit"

--- src/main/java/hackerrank/problemsolving/DayOfProgrammer.java
+++ src/main/java/hackerrank/problemsolving/DayOfProgrammer.java
@@ -14,16 +14,21 @@ public class DayOfProgrammer {
 
   private static String dayOfProgrammer(int year) {
     final String format = "dd.MM.yyyy";
-    if (year == 1918) return "26.09.1918";
-    else if (year < 1918) {
-      return year % 4 == 0
-              ? constDate(12, 9, year)
-              : constDate(13, 9, year);
-    } else {
-      return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
-              ? constDate(12, 9, year)
-              : constDate(13, 9, year);
-    }
+    if (year == 1918)     return "26.09.1918";
+    else if (year < 1918) return julian(year);
+    else                  return gregorian(year);
+  }
+
+  private static String julian(int year) {
+    return year % 4 == 0
+            ? constDate(12, 9, year)
+            : constDate(13, 9, year);
+  }
+
+  private static String gregorian(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+            ? constDate(12, 9, year)
+            : constDate(13, 9, year);
   }
 
   private static String constDate(int day, int month, int year) {
