9b5725ea1ff4a640fa8487567ffb71ec1aee52c6
Elvin <elvintaghiyev184@gmail.com>
2020-05-06 13:59:35 +0400

    "initial commit"

---
+++ src/main/java/hackerrank/problem_solving/EqualizeTheArray.java
@@ -0,0 +1,15 @@
+package hackerrank.problem_solving;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EqualizeTheArray {
+  public static void main(String[] args) {
+    System.out.println(equalizeArray(new int[]{3, 3, 2, 1, 3}));
+  }
+
+  private static int equalizeArray(int[] arr) {
+    Map<Integer, Integer> counts = new HashMap<>();
+    throw new IllegalArgumentException("Not implemented yet...");
+  }
+}
