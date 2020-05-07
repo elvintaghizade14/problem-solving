8462ef4d4c1c76db1cee5bd3986ba75a6efd6cd9
Elvin <elvintaghiyev184@gmail.com>
2020-05-07 10:24:08 +0400

    "initial commit"

--- src/main/java/hackerrank/problem_solving/SherlockAndSquares.java
+++ src/main/java/hackerrank/problem_solving/SherlockAndSquares.java
@@ -1,18 +1,15 @@
 package hackerrank.problem_solving;
 
-import java.util.stream.IntStream;
-
 public class SherlockAndSquares {
   public static void main(String[] args) {
     System.out.println(squares(3, 9));
     System.out.println(squares(17, 24));
+    System.out.println(squares(100, 1000));
   }
 
   private static int squares(int a, int b) {
-//    int s_a = (int) Math.ceil(Math.sqrt(a));
-//    int s_b = (int) Math.ceil(Math.sqrt(b));
-//    System.out.printf("s_a: %d, s_b:%d\n", s_a, s_b);
-//    return s_b - s_a + 1;
-    throw new IllegalArgumentException("Not implemented yet ...");
+    int s_a = (int) Math.ceil(Math.sqrt(a));
+    int s_b = (int) Math.floor(Math.sqrt(b));
+    return s_b - s_a + 1;
   }
 }
