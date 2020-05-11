066c19f28ae6b4be0b897c399f2c776865d1a3ff
Elvin <elvintaghiyev184@gmail.com>
2020-05-11 15:49:26 +0400

    "initial commit"

--- src/main/java/hackerrank/problem_solving/BetweenTwoSets.java
+++ src/main/java/hackerrank/problem_solving/BetweenTwoSets.java
@@ -3,12 +3,14 @@ package hackerrank.problem_solving;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class BetweenTwoSets {
   public static void main(String[] args) {
-//    throw new IllegalArgumentException("not implemented yet...");
     System.out.println(getTotalX(Arrays.asList(2, 4), Arrays.asList(16, 32, 96)));
     System.out.println(getTotalX(Collections.singletonList(2), Arrays.asList(20, 30, 12)));
+    System.out.println(getTotalX(Collections.singletonList(1), Collections.singletonList(100)));
+    throw new IllegalArgumentException("not implemented yet...");
   }
 
   private static int getTotalX(List<Integer> a, List<Integer> b) {
@@ -17,7 +19,10 @@ public class BetweenTwoSets {
     System.out.printf("lcm: %d, gcd:%d\n", A, B);
     int count = 0;
     for (int i = A; i <= B; i *= 2) {
-      if (B % A == 0) count++;
+      if (B % A == 0){
+        count++;
+        System.out.println(i);
+      }
     }
     return count;
   }
@@ -29,7 +34,8 @@ public class BetweenTwoSets {
   }
 
   private static int gcd(int a, int b) {
-    throw new IllegalArgumentException("should be implemented...");
+    if (b == 0) return a;
+    return gcd(b, a % b);
   }
 
   private static int lcm(List<Integer> in) {
@@ -39,6 +45,12 @@ public class BetweenTwoSets {
   }
 
   private static int lcm(int a, int b) {
-    throw new IllegalArgumentException("should be implemented...");
+    int max = Math.max(a, b);
+    int min = Math.min(a, b);
+    return IntStream.rangeClosed(1, min)
+            .filter(f -> (f * max) % min == 0)
+            .map(f -> Math.abs(f * max))
+            .findFirst()
+            .orElse(min * max);
   }
 }
