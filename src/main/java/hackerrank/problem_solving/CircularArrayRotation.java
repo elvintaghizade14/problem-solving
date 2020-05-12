7b1f693e884e5b1561a2509713351d8af5d37d0a
Elvin <elvintaghiyev184@gmail.com>
2020-05-12 16:26:29 +0400

    "initial commit"

--- src/main/java/hackerrank/problem_solving/CircularArrayRotation.java
+++ src/main/java/hackerrank/problem_solving/CircularArrayRotation.java
@@ -1,19 +1,35 @@
 package hackerrank.problem_solving;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class CircularArrayRotation {
   public static void main(String[] args) {
     circularArrayRotation(new int[]{3, 4, 5}, 2, new int[]{1});
+    circularArrayRotation(new int[]{3, 4, 5}, 2, new int[]{1, 2});
+    circularArrayRotation(new int[]{3, 4, 5}, 0, new int[]{0, 1, 2});
+    circularArrayRotation(new int[]{3, 4, 5}, 1, new int[]{0, 1, 2});
+    circularArrayRotation(new int[]{3, 4, 5}, 2, new int[]{0, 1, 2});
+    circularArrayRotation(new int[]{3, 4, 5}, 3, new int[]{0, 1, 2});
+  }
+
+  private static int[] circularArrayRotation1(int[] a, int k, int[] queries) {
+    int len = a.length;
+    int res_len = queries.length;
+    int[] res = new int[res_len];
+    k %= len;
+    for (int i = 0; i < res_len; i++) {
+      res[i] = a[(len - k + queries[i]) % len];
+    }
+    System.out.println(Arrays.toString(res));
+    return res;
   }
 
   private static int[] circularArrayRotation(int[] a, int k, int[] queries) {
-    int len = a.length;
-    int[] rotated = new int[len];
-
-    System.out.println(Arrays.toString(a));
-    System.out.println(Arrays.toString(rotated));
-//    return new int[]{1, 1};
-    throw new IllegalArgumentException("Not implemented...");
+    final int len = a.length;
+    final int k1 = len - k % len;
+    return Arrays.stream(queries)
+            .map(i -> a[(k1 + i) % len])
+            .toArray();
   }
 }
