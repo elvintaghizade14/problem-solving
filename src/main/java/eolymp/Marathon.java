fa9b7ead7feeb3556beb7395df6fbee8804782cf
e.taghizade@asan.gov.az <e.taghizade@asan.gov.az>
2021-03-03 10:50:03 +0400

    initial commit

---
+++ src/main/java/eolymp/Marathon.java
@@ -0,0 +1,10 @@
+package eolymp;
+
+import java.util.Scanner;
+
+public class Marathon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLong() - sc.nextLong());
+    }
+}
---
+++ src/main/java/eolymp/SumOfOddNumbers.java
@@ -0,0 +1,13 @@
+package eolymp;
+
+import java.util.Scanner;
+
+public class SumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+    }
+}
