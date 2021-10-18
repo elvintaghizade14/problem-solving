9bd1e3b87728f3ef6a5fd81b1ae287580a714100
elvintaghizade14 <elvintaghiyev184@gmail.com>
2021-10-18 16:36:23 +0400

    member inner class declaration example

---
+++ src/main/java/ocp/nested_classes/MemberInnerClass.java
@@ -0,0 +1,21 @@
+package ocp.nested_classes;
+
+public class MemberInnerClass {
+    private final String GREETING = "Hi";
+
+    protected class Inner {
+        public final int repeat = 3;
+
+        public void go() {
+            for (int i = 0; i < repeat; i++) System.out.println(GREETING);
+        }
+    }
+
+    public void callInner() {
+        new Inner().go();
+    }
+
+    public static void main(String[] args) {
+        new MemberInnerClass().callInner();
+    }
+}
