ff9f415e60e389bc71c4b54af64ea47acd54aa88
elvintaghizade14 <elvintaghiyev184@gmail.com>
2021-10-19 10:33:54 +0400

    anonymous inner class implementation

---
+++ src/main/java/ocp/nested_classes/anonymous_inner/AnonymousInnerClass.java
@@ -0,0 +1,18 @@
+package ocp.nested_classes.anonymous_inner;
+
+
+public class AnonymousInnerClass {
+
+    abstract class SaleTodayOnly {
+        abstract int dollarsOff();
+    }
+
+    public int admission(int basePrice) {
+        final SaleTodayOnly sale = new SaleTodayOnly() {
+            int dollarsOff() {
+                return 3;
+            }
+        };
+        return basePrice - sale.dollarsOff();
+    }
+}
