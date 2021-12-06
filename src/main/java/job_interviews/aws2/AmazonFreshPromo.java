bf45b3cbbe7b4c967466ce98e97f0100861da0bd
elvintaghizade14 <elvintaghiyev184@gmail.com>
2021-12-06 22:53:47 +0400

    amazon fresh promo added

---
+++ src/main/java/job_interviews/aws2/AmazonFreshPromo.java
@@ -0,0 +1,9 @@
+package job_interviews.aws2;
+
+import java.util.List;
+
+public class AmazonFreshPromo {
+    public static int foo(List<String> codeList, List<String> shoppingCart) {
+        return 0;
+    }
+}
