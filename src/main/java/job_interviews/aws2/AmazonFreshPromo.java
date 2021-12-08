537b254ca7f737b38e6f4ed9d9be2dcd60eeb72b
elvintaghizade14 <elvintaghiyev184@gmail.com>
2021-12-08 21:11:22 +0400

    amazon fresh promo example exercise intro added

--- src/main/java/job_interviews/aws2/AmazonFreshPromo.java
+++ src/main/java/job_interviews/aws2/AmazonFreshPromo.java
@@ -1,9 +1,36 @@
 package job_interviews.aws2;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AmazonFreshPromo {
+    private static final String SPACE = " ";
+
+    public static void main(String[] args) {
+        System.out.println(foo(
+                Collections.singletonList("apple apple banana anything banana"),
+                Arrays.asList("orange", "apple", "apple", "banana", "orange", "banana")
+        ));
+
+        System.out.println(foo(
+                Collections.singletonList("apple apple banana anything banana"),
+                Arrays.asList("banana", "orange", "banana", "apple", "apple")
+        ));
+
+        System.out.println(foo(
+                Collections.singletonList("apple apple banana anything banana"),
+                Arrays.asList("apple", "banana", "apple", "banana", "orange", "banana")
+        ));
+    }
+
     public static int foo(List<String> codeList, List<String> shoppingCart) {
+        if (codeList.isEmpty()) return 1;
+
+        final List<List<String>> secretCodes = codeList.stream().map(secretGroup -> Arrays.asList(secretGroup.split(SPACE))).collect(Collectors.toList());
+        int secretIndex = 0;
+        boolean consistent = true;
         return 0;
     }
 }
