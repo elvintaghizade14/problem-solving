43a30c8314a3a009167be3e928a554c6238a95c6
Elvin Taghizade <elvintaghiyev184@gmail.com>
2022-02-16 11:40:05 +0400

    Create FindMaxConsecutiveOnes.java

---
+++ src/main/java/leetcode/array/FindMaxConsecutiveOnes.java
@@ -0,0 +1,13 @@
+package com.leetcode.array;
+
+public class FindMaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num == 0) count = 0;
+            else max = Math.max(max, ++count);
+        }
+        return max;
+    }
+}
