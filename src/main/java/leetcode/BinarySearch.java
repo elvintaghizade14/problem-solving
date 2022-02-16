88a8f85f126656967f07f192327c76bff385e315
Elvin Taghizade <elvintaghiyev184@gmail.com>
2022-02-16 11:40:37 +0400

    Create BinarySearch.java

---
+++ src/main/java/leetcode/BinarySearch.java
@@ -0,0 +1,17 @@
+package com.leetcode.binary_search;
+
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+}
